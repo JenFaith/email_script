6701f1462dc5f1105b51f0e2e35fc5eb7c55a11d
JenFaith <jennifer.faith16@gmail.com>
2021-08-02 15:17:59 -0400

    Updates to html

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-import pandas
-import numpy
-from flask import Flask, render_template, request
+# IMPORT STATEMENTS
+from flask import Flask, render_template, request, session
+import pandas as pd
+# import numpy as np
+import jinja2
+# import requests
+# import re
 
 def create_app():
     app = Flask(__name__)
     
-    #Testing route
     @app.route('/')
     def home():
         return render_template('email.html')
+    
+    #Email Route
+    @app.route('/email')
+    def email():
+        first_name=request.args.get('fname')
+        return render_template('email.html', first_name=first_name)
+    
+    
+
+
+
+
+
 
     return app
 
